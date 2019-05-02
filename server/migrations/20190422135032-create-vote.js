595d9aa736477d4a4202bc0df7bf24e0055e3673
Mary Mazi <mazi.mary.o@gmail.com>
2019-05-02 01:53:10 +0100

    add migration files

---
+++ server/migrations/20190422135032-create-vote.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Votes', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      createdBy: {
+        type: Sequelize.UUID
+      },
+      office: {
+        type: Sequelize.INTEGER
+      },
+      candidate: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Votes');
+  }
+};
