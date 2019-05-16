b5a917f1bba243dbb1bbd347f5ba14ef500b7d90
Mary Mazi <mazi.mary.o@gmail.com>
2019-05-16 13:29:57 +0100

    update database config to load DB URL from environment variable

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,6 +1,8 @@
 import expressValidator from 'express-validator';
 import routes from './routes';
 
+require('dotenv').config();
+
 //  Load the required middlewares and routes for v1
 export default (app, express) => {
   app.use(express.json());
