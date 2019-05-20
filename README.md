1ebe5085392aac0b7098a7210f8f54f9302b8fc9
Mary Mazi <mazi.mary.o@gmail.com>
2019-05-20 11:08:22 +0100

    chore(test and ci):
    - add more test cases to increase coverage

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 [![Coverage Status](https://coveralls.io/repos/github/mazma1/politico/badge.svg?branch=develop)](https://coveralls.io/github/mazma1/politico?branch=develop)
+[![codecov](https://codecov.io/gh/mazma1/politico/branch/develop/graph/badge.svg)](https://codecov.io/gh/mazma1/politico)
---
+++ server/tests/app.test.js
@@ -0,0 +1,28 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app';
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('App.js', function () {
+  it('should return status 400 if specified version in request URL does not exist', function (done) {
+    chai.request(app)
+      .get('/api/v0/')
+      .end((err, res) => {
+        res.status.should.equal(404);
+        res.body.should.have.property('message').eql('The requested endpoint does not exist');
+        done();
+      });
+  });
+
+  it('should return status 200 and a message that client was loaded if base URL is specified', function (done) {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        res.status.should.equal(200);
+        res.body.should.have.property('message').eql('Loaded the client app');
+        done();
+      });
+  });
+});
